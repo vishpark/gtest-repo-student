b47ef22c25225e911465a565d7a4ab50fed06814
anonymous_insaan <56506799+vishpark@users.noreply.github.com>
2024-04-22 16:22:16 +0530

    Update StringCalculator.Tests.cpp
    frfe

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -4,8 +4,18 @@
 TEST(StringCalculatorTestSuite,GivenEmptyStringZeroIsExpected){
     //Arrange
     StringCalculator objUnderTest;
-    string input="-6,-5,4,3";
-    int expectedValue=7;
+    string input="";
+    int expectedValue=0;
+    //Act
+   int actualValue=  objUnderTest.Add(input);
+    //Assert
+    ASSERT_EQ(actualValue,expectedValue);
+}
+TEST(StringCalculatorTestSuite,GivenStringHasNegative){
+    //Arrange
+    StringCalculator objUnderTest;
+    string input="1,2,-3";
+    int expectedValue=3;
     //Act
    int actualValue=  objUnderTest.Add(input);
     //Assert
