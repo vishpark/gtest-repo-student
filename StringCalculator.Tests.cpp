53451b0ed8ab126784beb55399d964cb9cf51099
Venugopal K <venu.shastri@gmail.com>
2024-04-22 15:22:07 +0530

    Create StringCalculator.Tests.cpp

---
+++ StringCalculator.Tests.cpp
@@ -0,0 +1,13 @@
+#include "StringCalculator.h"
+#include <gtest/gtest.h>
+
+TEST(StringCalculatorTestSuite,GivenEmptyStringZeroIsExpected){
+    //Arrange
+    StringCalculator objUnderTest;
+    string input="";
+    int expectedValue=0;
+    //Act
+   int actualValue=  objUnderTest.Add(input);
+    //Assert
+    ASSERT_EQ(actualValue,expectedValue);
+}
